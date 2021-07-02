a68fa2d505c9a62c94dbae06255720915655d31c
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-07-02 18:25:22 +0530

    skill page updatet

---
+++ src/Skill/SkillData.js
@@ -0,0 +1,166 @@
+import HTML5 from "../Assets/images/html-5.svg";
+import CSS3 from "../Assets/images/css3.svg";
+import SASS from "../Assets/images/sass-1.svg";
+import BOOTSTRAP from "../Assets/images/bootstrap-4.svg";
+import REACT from "../Assets/images/react.svg";
+import REACT_ROUTER from "../Assets/images/react-router.svg";
+import MATERIALUI from "../Assets/images/material-ui-1.svg";
+import REACT_BOOTSTRAP from "../Assets/images/react-bootstrap.svg";
+import NODE_JS from "../Assets/images/nodejs.svg";
+import MONGODB from "../Assets/images/mongodb.svg";
+import GIT from "../Assets/images/git-icon.svg";
+import HEROKU from "../Assets/images/heroku.svg";
+import GITHUB_PAGES from "../Assets/images/github.svg";
+import NETLIFY from "../Assets/images/netlify.svg";
+import GODADDY from "../Assets/images/GoDaddy.svg";
+import JAVASCRIPT from "../Assets/images/javascript.svg";
+import PYTHON from "../Assets/images/python.svg";
+import CPP from "../Assets/images/c++.svg";
+import C from "../Assets/images/c-program.svg";
+
+
+
+
+export const skills= {
+    frontend: [
+      {
+        link: "https://en.wikipedia.org/wiki/HTML5",
+        imgAltText: "HTML 5",
+        imgSrc: HTML5,
+        skillName: "HTML5",
+      },
+      {
+        link: "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+        imgAltText: "CSS 3",
+        imgSrc: CSS3,
+        skillName: "CSS3",
+      },
+      {
+        link: "https://sass-lang.com/",
+        imgAltText: "Sass",
+        imgSrc: SASS,
+        skillName: "Sass",
+      },
+      {
+        link: "https://getbootstrap.com/",
+        imgAltText: "Bootstrap",
+        imgSrc: BOOTSTRAP,
+        skillName: "Bootstrap",
+      },
+      {
+        link: "https://reactjs.org/",
+        imgAltText: "React JS",
+        imgSrc: REACT,
+        skillName: "React JS",
+      },
+      
+      {  
+     
+        link: "https://reacttraining.com/react-router/",
+        imgAltText: "React Router",
+        imgSrc: REACT_ROUTER,
+        skillName: "React Router",
+      },
+      {
+        link: "https://material-ui.com/",
+        imgAltText: "Material-UI",
+        imgSrc: MATERIALUI,
+        skillName: "Material-UI",
+      },
+      {
+        link: "https://react-bootstrap.github.io/",
+        imgAltText: "React Bootstrap",
+        imgSrc: REACT_BOOTSTRAP,
+        skillName: "React Bootstrap",
+      },
+     
+      
+    ],
+  
+    backend: [
+      {
+        link: "https://nodejs.org/en/",
+        imgAltText: "Node.js",
+        imgSrc: NODE_JS,
+        skillName: "Node.js",
+      },
+  
+   
+    ],
+    hostingPlatforms: [
+      {
+        link: "https://www.heroku.com/",
+        imgAltText: "Heroku",
+        imgSrc: HEROKU,
+        skillName: "Heroku",
+      },
+   
+      {
+        link: "https://pages.github.com/",
+        imgAltText: "GitHub Pages",
+        imgSrc: GITHUB_PAGES,
+        skillName: "GitHub Pages",
+      },
+      
+      {
+        link: "https://www.godaddy.com/",
+        imgAltText: "GoDaddy",
+        imgSrc: GODADDY,
+        skillName: "GoDaddy",
+      },
+      {
+        link: "https://www.netlify.com/",
+        imgAltText: "Netlify",
+        imgSrc: NETLIFY,
+        skillName: "Netlify",
+      },
+    ],
+    
+    programmingLanguages: [
+      {
+        link: "https://www.javascript.com/",
+        imgAltText: "JavaScript",
+        imgSrc: JAVASCRIPT,
+        skillName: "JavaScript",
+      },
+     
+      {
+        link: "https://www.python.org/",
+        imgAltText: "Python",
+        imgSrc: PYTHON,
+        skillName: "Python",
+      },
+      {
+        link: "https://devdocs.io/c/",
+        imgAltText: "C-lang",
+        imgSrc: C,
+        skillName: "C-langage",
+      },
+      {
+        link: "https://devdocs.io/cpp/",
+        imgAltText: "C++",
+        imgSrc: CPP,
+        skillName: "C++",
+      },
+   
+    ],
+    databases: [
+     
+  
+      {
+        link: "https://www.mongodb.com/",
+        imgAltText: "MongoDB",
+        imgSrc: MONGODB,
+        skillName: "MongoDB",
+      },
+    ],
+    versionControl: [
+      {
+        link: "https://git-scm.com/",
+        imgAltText: "GIT",
+        imgSrc: GIT,
+        skillName: "GIT",
+      },
+    ],
+  };
+  
