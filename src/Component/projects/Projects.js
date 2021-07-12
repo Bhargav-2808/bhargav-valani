6e98818552bdf5df40ec2d54a8c3ce802869baaa
Bhargav-2808 <bhargavvalani01@gmail.com>
2021-07-12 16:22:24 +0530

    project added

--- src/Component/projects/Projects.js
+++ src/Component/projects/Projects.js
@@ -120,6 +120,80 @@ export default function Projects() {
                   </Accordion>
                 </div>
               </div>
+              
+              {/* Portfolio Website */}
+              <div className="d-flex justify-content-between flex-column mt-1">
+                <div>
+                  <Accordion>
+                    <Card>
+                      <Accordion.Toggle
+                        as={Card.Header}
+                        eventKey="0"
+                        className="p-2 text-center accordian-main"
+                      >
+                        PROJECT DETAILS - PORTFOLIO WEBSITE
+                      </Accordion.Toggle>
+
+                      <Accordion.Collapse eventKey="0" className="text-left">
+                        <Card.Body>
+                          <strong>Description:</strong> The Website Simply made using React Components to reduce the responce time.
+                          <hr />
+                          
+                     
+                          <strong>Tech used:</strong>
+                          <ul>
+                            <li>
+                              <span className="p-2"> REACT</span>
+                            </li>
+                            <li>
+                              <span className="p-2"> HTML</span>
+                            </li>
+                            <li>
+                              <span className="p-2"> CSS</span>
+                            </li>
+                            <li>
+                              <span className="p-2"> NPM PACKAGE</span>
+                            </li>
+                            <li>
+                              <span className="p-2"> react-bootstrap</span>
+                            </li>
+                            <li>
+                              <span className="p-2">MATERIAL-UI</span>
+                            </li>
+                            <li>
+                              <span className="p-2">GITHUB PAGES</span>
+                            </li>
+                          </ul>
+                          <div className="d-flex justify-content-between flex-nowrap text-center">
+                            <Button variant="">
+                              <a
+                                href="https://github.com/Bhargav-2808/bhargav-valani/"
+                                style={{
+                                  textDecoration: "none",
+                                  color: "#37474f",
+                                }}
+                              >
+                                SOURCE CODE
+                              </a>
+                            </Button>
+                            <Button variant="">
+                              <a
+                                href="https://bhargav-2808.github.io/bhargav-valani/"
+                                style={{
+                                  textDecoration: "none",
+                                  color: "#37474f",
+                                }}
+                              >
+                                SEE LIVE
+                              </a>
+                            </Button>
+                          </div>
+                        </Card.Body>
+                      </Accordion.Collapse>
+                    </Card>
+                  </Accordion>
+                </div>
+              </div>
 
               {/* Project: Freelancing*/}
               <div className="d-flex justify-content-between flex-column mt-1">
@@ -152,11 +226,14 @@ export default function Projects() {
                             <li>
                               <span className="p-2">BOOTSRAP</span>
                             </li>
+                            <li>
+                              <span className="p-2">FONT-AWESOME</span>
+                            </li>
                           </ul>
                           <div className="d-flex justify-content-between flex-nowrap text-center">
                             <Button variant="">
                               <a
-                                href="#"
+                                href="https://www.ktsglamstation.ca/"
                                 style={{
                                   textDecoration: "none",
                                   color: "#37474f",
@@ -204,11 +281,14 @@ export default function Projects() {
                             <li>
                               <span className="p-2">BOOTSRAP</span>
                             </li>
+                            <li>
+                              <span className="p-2">FONT-AWESOME</span>
+                            </li>
                           </ul>
                           <div className="d-flex justify-content-between flex-nowrap text-center">
                             <Button variant="">
                               <a
-                                href="#"
+                                href="https://kanutechnologies.ca/"
                                 style={{
                                   textDecoration: "none",
                                   color: "#37474f",
